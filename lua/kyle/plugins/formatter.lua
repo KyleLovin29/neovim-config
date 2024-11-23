ef3f51f65fe7442065d29e1f49558dd2a7d82897
KyleLovin29 <ky1e1ovin4@gmail.com>
2024-11-23 10:14:27 -0700

    Changed formatter to conform.nvim

--- lua/kyle/plugins/formatter.lua
+++ lua/kyle/plugins/formatter.lua
@@ -1,3 +1,26 @@
 return {
-  "mhartington/formatter.nvim"
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+
+  config = function ()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" }, 
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      },
+    })
+  end
 }
